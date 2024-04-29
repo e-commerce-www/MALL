87eb4e14cc1831d8fe9d319612136c1540c6a27c
madEffort <madeffort0903@gmail.com>
2024-04-29 14:05:15 +0900

    feat: stream song mp3

--- apps/oauth/views.py
+++ apps/oauth/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from apps.orders.models import Order
 
 # Create your views here.
 def profile(request):
     return render(request, 'accounts/profile.html')
 
+@login_required
 def purchase(request):
-    return render(request, 'accounts/purchase_detail.html')
+    orders = Order.objects.filter(user=request.user,payment__is_paid=True).order_by('-id')
+    
+    return render(request, 'accounts/purchase_detail.html', context={'orders': orders})
 
 def sales(request):
     return render(request, 'accounts/sales_detail.html')
--- apps/songs/templatetags/session_filters.py
+++ apps/songs/templatetags/session_filters.py
@@ -1,8 +1,14 @@
 from django import template
 from apps.songs.models import Song
+from apps.orders.models import Order
+from django.contrib.auth import get_user_model
 
 register = template.Library()
 
 @register.filter
 def get_song(song_id):
     return Song.objects.get(id=song_id)
+
+@register.simple_tag
+def is_purchased(user, song_id):
+    return Order.objects.filter(user=user, payment__is_paid=True, song__id=song_id).exists()
