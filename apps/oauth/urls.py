799ae978fe908d8b80d17cc38ff73e758465f2e8
Tobias Kim <madeffort0903@gmail.com>
2024-05-02 14:53:58 +0900

    fix: song list

--- apps/oauth/urls.py
+++ apps/oauth/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('purchase/', views.purchase, name='purchase'),
     path('sales/', views.sales, name='sales'),
-    path('download/', views.download, name='download'),
     path('follow/', views.follow, name='following'),
     path('unfollow/<int:pk>/', views.unfollow, name='unfollow'),
 ]
