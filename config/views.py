703920d07770d8a33a1dae1e667b61ebc464cfe9
kmj <kimmj7916@gmail.com>
2024-05-02 10:32:06 +0900

    add_search

--- config/views.py
+++ config/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse
 from apps.songs.models import Song
 from django.http import JsonResponse
 from config.forms import SearchForm
@@ -18,9 +19,25 @@ def custom_404(request, exception):
 
 
 def search(request):
-    if request.method =='POST':
-        data=json.loads(request.body.decode('utf-8'))
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        search_title = data.get('title')
         
-        name= data.get('name')
-        
-        return JsonResponse({'success':name})
+        search_results = Song.objects.filter(title__icontains=search_title).distinct()
+        if search_results:
+            search_html = "<ul class='search-results'>"
+            for song in search_results:
+                search_html += "<li class='search-item'>"
+                search_html += "<a href='{}'>".format(reverse('songs:song_detail', args=[song.id]))
+                search_html += "<img src='{}' alt='{}' class='search-thumbnail'>".format(song.thumbnail.url, song.title)
+                search_html += "<h3 class='search-title'>{}</h3>".format(song.title)
+                search_html += "<p class='search-seller'>seller -  {}</p>".format(song.seller)
+                search_html += "</a>"
+                search_html += "</li>"
+            search_html += "</ul>"
+            
+            return JsonResponse({'success': 'true', 'data': search_html})
+        else:
+            return JsonResponse({'success': 'false', 'message': '검색 결과가 없습니다.'})
+    else:
+        return JsonResponse({'error': 'Invalid request method'})
