c22ebe1064ff9d921cf9ede3202d45f67a32e3f1
Tobias Kim <madeffort0903@gmail.com>
2024-05-03 17:59:12 +0900

    fix: sales detail time text to korean .

--- config/settings.py
+++ config/settings.py
@@ -168,9 +168,9 @@ AUTH_USER_MODEL = "oauth.User"
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
